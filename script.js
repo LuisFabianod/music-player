fff0b25c6521b8d64066d0594b5c4aba7ab90700
luisfabiano <luisfabianodev@gmail.com>
2024-05-11 22:44:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const musics = [
+    {
+        name: "Hit 'Em Up",
+        artist: "2Pac",
+        audioSrc: "./audios/hitemup.mp3",
+        imageSrc:"./images/hitThemUpIcon.png",
+    },
+    {
+        name: "Lady Brown",
+        artist: "Nujabes Ft. Cise Starr",
+        audioSrc: "./audios/ladybrown.mp3",
+        imageSrc:"./images/ladyBrownIcon.png",
+    },
+
+]
