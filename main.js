826a0c1e2c380171352f0be16c33e06e5147c7cf
luisfabiano <luisfabianodev@gmail.com>
2024-05-13 13:00:26 -0300

    finish template

---
+++ main.js
@@ -0,0 +1,7 @@
+const musics = window.musics;
+
+const pOrder = document.querySelector('.order');
+const pName = document.querySelector('.music-name');
+const pArtist = document.querySelector('.music-artist');
+const divImg = document.querySelector('.music-img');
+
--- musics.js
+++ musics.js
@@ -1,75 +1,75 @@
-const musics = [
+window.musics = [
     {
         name: "Hit 'Em Up",
         artist: "2Pac",
         audioSrc: "./audios/hitemup.mp3",
-        imageSrc:"./images/hitThemUpIcon.png",
+        imageSrc:"./music-images/hitThemUpIcon.png",
     },
     {
         name: "Lady Brown",
         artist: "Nujabes Ft. Cise Starr",
         audioSrc: "./audios/ladybrown.mp3",
-        imageSrc:"./images/ladyBrownIcon.png",
+        imageSrc:"./music-images/ladyBrownIcon.png",
     },
     {
         name: "Bring Me To Life",
         artist: "Evanescence",
         audioSrc: "./audios/bringmetolife.mp3",
-        imageSrc:"./images/bringMeToLifeIcon.png",
+        imageSrc:"./music-images/bringMeToLifeIcon.png",
     },
     {
         name: "Cherry Waves",
         artist: "Deftones",
         audioSrc: "./audios/cherrywaves.mp3",
-        imageSrc:"./images/cherryWavesIcon.png",
+        imageSrc:"./music-images/cherryWavesIcon.png",
     },
     {
         name: "Enchanted",
         artist: "Taylor Swift",
         audioSrc: "./audios/enchanted.mp3",
-        imageSrc:"./images/enchantedIcon.png",
+        imageSrc:"./music-images/enchantedIcon.png",
     },
     {
         name: "Runaway",
         artist: "Kanye West",
         audioSrc: "./audios/runaway.mp3",
-        imageSrc:"./images/runawayIcon.png",
+        imageSrc:"./music-images/runawayIcon.png",
     },
     {
         name: "Entidade",
         artist: "Did Brock",
         audioSrc: "./audios/entidade.mp3",
-        imageSrc:"./images/entidadeIcon.png",
+        imageSrc:"./music-images/entidadeIcon.png",
     },
     {
         name: "Violence",
         artist: "Oliver Francis",
         audioSrc: "./audios/violence.mp3",
-        imageSrc:"./images/violenceIcon.png",
+        imageSrc:"./music-images/violenceIcon.png",
     },
     {
         name: "Duvet",
         artist: "Bôa",
         audioSrc: "./audios/duvet.mp3",
-        imageSrc:"./images/duvetIcon.png",
+        imageSrc:"./music-images/duvetIcon.png",
     },
     {
         name: "After Dark",
         artist: "Mr.Kitty",
         audioSrc: "./audios/afterdark.mp3",
-        imageSrc:"./images/afterDarkIcon.png",
+        imageSrc:"./music-images/afterDarkIcon.png",
     },
     {
         name: "Dead Presidents II",
         artist: "JAY-Z",
         audioSrc: "./audios/deadpresidents.mp3",
-        imageSrc:"./images/deadPresidentsIcon.png",
+        imageSrc:"./music-images/deadPresidentsIcon.png",
     },
     {
         name: "FOTO COM A MÃO NA CARA",
         artist: "tchelo rodrigues",
         audioSrc: "./audios/fotocomamaonacara.mp3",
-        imageSrc:"./images/fotoComAMaoNaCaraIcon.png",
+        imageSrc:"./music-images/fotoComAMaoNaCaraIcon.png",
     },
 
 ]   
