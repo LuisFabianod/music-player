612ebd0093ada254852abe2a74a1423717fca31b
luisfabiano <luisfabianodev@gmail.com>
2024-05-13 10:09:40 -0300

    add musics

--- script.js
+++ script.js
@@ -11,5 +11,65 @@ const musics = [
         audioSrc: "./audios/ladybrown.mp3",
         imageSrc:"./images/ladyBrownIcon.png",
     },
+    {
+        name: "Bring Me To Life",
+        artist: "Evanescence",
+        audioSrc: "./audios/bringmetolife.mp3",
+        imageSrc:"./images/bringMeToLifeIcon.png",
+    },
+    {
+        name: "Cherry Waves",
+        artist: "Deftones",
+        audioSrc: "./audios/cherrywaves.mp3",
+        imageSrc:"./images/cherryWavesIcon.png",
+    },
+    {
+        name: "Enchanted",
+        artist: "Taylor Swift",
+        audioSrc: "./audios/enchanted.mp3",
+        imageSrc:"./images/enchantedIcon.png",
+    },
+    {
+        name: "Runaway",
+        artist: "Kanye West",
+        audioSrc: "./audios/runaway.mp3",
+        imageSrc:"./images/runawayIcon.png",
+    },
+    {
+        name: "Entidade",
+        artist: "Did Brock",
+        audioSrc: "./audios/entidade.mp3",
+        imageSrc:"./images/entidadeIcon.png",
+    },
+    {
+        name: "Violence",
+        artist: "Oliver Francis",
+        audioSrc: "./audios/violence.mp3",
+        imageSrc:"./images/violenceIcon.png",
+    },
+    {
+        name: "Duvet",
+        artist: "Bôa",
+        audioSrc: "./audios/duvet.mp3",
+        imageSrc:"./images/duvetIcon.png",
+    },
+    {
+        name: "After Dark",
+        artist: "Mr.Kitty",
+        audioSrc: "./audios/afterdark.mp3",
+        imageSrc:"./images/afterDarkIcon.png",
+    },
+    {
+        name: "Dead Presidents II",
+        artist: "JAY-Z",
+        audioSrc: "./audios/deadpresidents.mp3",
+        imageSrc:"./images/deadPresidentsIcon.png",
+    },
+    {
+        name: "FOTO COM A MÃO NA CARA",
+        artist: "tchelo rodrigues",
+        audioSrc: "./audios/fotocomamaonacara.mp3",
+        imageSrc:"./images/fotoComAMaoNaCaraIcon.png",
+    },
 
-]
+]   
